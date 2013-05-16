11fdd3417f2972bf8d670cc5d25d0bb277d7a006
jeje <jeremy.beaucousin@gmail.com>
2013-05-17 00:50:58 +0200

    Replace the Jframe by a window

--- Media_Server_application_test/appClientModule/vue/MainWindow.java
+++ Media_Server_application_test/appClientModule/vue/MainWindow.java
@@ -6,24 +6,25 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
-import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JWindow;
 
-public class MainWindow extends JDialog implements ActionListener{
+public class MainWindow extends JWindow implements ActionListener{
 	FileKindSelection fileKindSelection;
 	JPanel jpanel;
 	public MainWindow() {
-		this.setTitle("Media Server Application"); //On donne un titre à l'application
+		//this.setTitle("Media Server Application"); //On donne un titre à l'application
 		this.setSize(300, 200);//On lui donne une taille pour qu'on puisse la voir
 		this.setVisible(true);//On la rend visible
 		this.setLocationRelativeTo(null);//On centre la fenêtre sur l'écran
-		this.setResizable(false); //On interdit la redimensionnement de la fenêtre
-		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); //On dit à l'application de se fermer lors du clic sur la croix
+		//this.setResizable(false); //On interdit la redimensionnement de la fenêtre
+		//this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); //On dit à l'application de se fermer lors du clic sur la croix
 		this.fileKindSelection = new FileKindSelection(this);
 		System.out.println(fileKindSelection);
 		this.setContentPane(fileKindSelection);
+		this.setVisible(true);
 	}
 
 	public void getFileKindSelectionParam(Boolean videoIsSelected, Boolean serieIsSelected, Boolean musicIsSelected) {
