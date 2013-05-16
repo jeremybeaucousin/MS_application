f8db5b1c41a687bc41c0a6076d5575639aa6e6cd
jbeaucousin <jbeaucousin@FRW022474.pe.sterianet>
2013-05-16 14:05:53 +0200

    Test sur la première fenetre

--- Media_Server_application_test/appClientModule/Main.java
+++ Media_Server_application_test/appClientModule/Main.java
@@ -1,15 +1,18 @@
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.io.File;
 import java.io.IOException;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 
+import javax.swing.JButton;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JDialog;
 import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import org.json.JSONException;
-
-import Model.Document;
-import Model.Movie;
-import Model.TheMovieDB;
 
 public class Main{
 	
@@ -21,6 +24,53 @@ public class Main{
 		chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 		chooser.setAcceptAllFileFilterUsed(false);
 		
+		JDialog dialog = new JDialog(); 
+		dialog.setTitle("Media Server Application"); //On donne un titre à l'application
+		dialog.setSize(300, 200);//On lui donne une taille pour qu'on puisse la voir
+		dialog.setVisible(true);//On la rend visible
+		dialog.setLocationRelativeTo(null);//On centre la fenêtre sur l'écran
+		dialog.setResizable(false); //On interdit la redimensionnement de la fenêtre
+		dialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); //On dit à l'application de se fermer lors du clic sur la croix
+
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridBagLayout());
+		GridBagConstraints gridBagConstraints = new GridBagConstraints();
+		
+		gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
+		gridBagConstraints.weighty = 1;
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 0;
+		JLabel label = new JLabel("Quel média voulez-vous scannez : ");
+		panel.add(label, gridBagConstraints);
+		
+		gridBagConstraints.fill = GridBagConstraints.CENTER;
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 1;
+		JCheckBoxMenuItem checkbox1 = new JCheckBoxMenuItem();
+		checkbox1.setText("Film");
+		panel.add(checkbox1, gridBagConstraints);
+		
+		gridBagConstraints.fill = GridBagConstraints.CENTER;
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 2;
+		JCheckBoxMenuItem checkbox2 = new JCheckBoxMenuItem();
+		checkbox2.setText("Série");
+		panel.add(checkbox2, gridBagConstraints);
+		
+		gridBagConstraints.fill = GridBagConstraints.CENTER;
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 3;
+		JCheckBoxMenuItem checkbox3 = new JCheckBoxMenuItem();
+		checkbox3.setText("Musique");
+		panel.add(checkbox3, gridBagConstraints);
+		
+		gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 4;
+		JButton bouton = new JButton("Cliquez ici !");
+		panel.add(bouton, gridBagConstraints);
+		
+		dialog.setContentPane(panel);
 //        //Create and set up the window.
 //        JFrame frame = new JFrame("TopLevelDemo");
 //        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -112,27 +162,27 @@ public class Main{
 //		}	
 		
 		// FOR THE MOVIE DB		
-		if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-			chooser.setCurrentDirectory(chooser.getSelectedFile());
-			filesList = Document.FolderScannerVideo(chooser.getCurrentDirectory());
-		}
-		if(filesList != null) {
-			for(int ii = 0; ii < filesList.size(); ii++) {
-				Movie movie = new Movie(filesList.get(ii).getName(), filesList.get(ii).getAbsolutePath());
-				Integer movieId = TheMovieDB.searchMovieStudying(movie);
-				System.out.println(new DecimalFormat("###.##").format(new Double(ii)/filesList.size()*100) + "%");
-				if(movieId < 0 ) {
-					cpt++;
-				} else {
-					System.out.println(TheMovieDB.getMovie(movieId).get("original_title"));
-				}
-			}
-			
-			System.out.println("total de film : " + filesList.size());
-			System.out.println("total reconnus : " + (filesList.size() - cpt));
-			System.out.println("total non reconnus : " + (cpt));
-			System.out.println("Ratio de réussite : " + (new DecimalFormat("###.##").format(new Double((filesList.size() - cpt))/filesList.size()*100) + "%"));
-		}
+//		if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+//			chooser.setCurrentDirectory(chooser.getSelectedFile());
+//			filesList = Document.FolderScannerVideo(chooser.getCurrentDirectory());
+//		}
+//		if(filesList != null) {
+//			for(int ii = 0; ii < filesList.size(); ii++) {
+//				Movie movie = new Movie(filesList.get(ii).getName(), filesList.get(ii).getAbsolutePath());
+//				Integer movieId = TheMovieDB.searchMovieStudying(movie);
+//				System.out.println(new DecimalFormat("###.##").format(new Double(ii)/filesList.size()*100) + "%");
+//				if(movieId < 0 ) {
+//					cpt++;
+//				} else {
+//					System.out.println(TheMovieDB.getMovie(movieId).get("original_title"));
+//				}
+//			}
+//			
+//			System.out.println("total de film : " + filesList.size());
+//			System.out.println("total reconnus : " + (filesList.size() - cpt));
+//			System.out.println("total non reconnus : " + (cpt));
+//			System.out.println("Ratio de réussite : " + (new DecimalFormat("###.##").format(new Double((filesList.size() - cpt))/filesList.size()*100) + "%"));
+//		}
 		
 		
 		//request exemple http://api.themoviedb.org/3/search/movie?api_key=1acc7c1593ee8145d2d390f1d419a573&query=shrek
